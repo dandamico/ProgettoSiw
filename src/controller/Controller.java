c5b85c6117b6e955c346f8593fae7904cd2ffdda
dandamico <danieledamico16@gmail.com>
2019-05-20 16:37:56 +0200

    Commit3

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,18 @@
 package controller;
 
 public class Controller {
-
+	private int x;
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 }
