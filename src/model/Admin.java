c79b0a06797d3a195627111e2e482aef16be4c5c
dandamico <danieledamico16@gmail.com>
2019-05-23 10:37:14 +0200

    Commit21

---
+++ src/model/Admin.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Admin {
+	private String nome;
+	private String cognome;
+	private String password;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
