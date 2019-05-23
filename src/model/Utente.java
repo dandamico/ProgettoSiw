36155a02ee9219298a216c849534485ddc3595b2
dandamico <danieledamico16@gmail.com>
2019-05-23 10:49:03 +0200

    Commit22

---
+++ src/model/Utente.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Utente {
+	
+	private String nome;
+	private String cognome;
+	private LocalDate dataDiNascita;
+	
+	private Prodotto prodotto;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+	
+	
+}
